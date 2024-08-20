cc58cf5cf8998e789f8f54bbf7d33267c6ea4816
Chandika Herath <chandikasbh@gmail.com>
2024-08-20 20:52:04 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello IndustryConnect!");
+
+        // Open Chrome Browser
+        IWebDriver driver = new ChromeDriver();
+
+        // Launch TurnUp portal
+        driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+        driver.Manage().Window.Maximize();
+        Thread.Sleep(1000);
+
+        // Identify username textbox and enter valid username
+        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+        usernameTextbox.SendKeys("hari");
+
+
+        // Identify password textbox and enter valid password
+        IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+        passwordTextbox.SendKeys("123123");
+
+        // Identify login buton and click on it
+        IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+        loginButton.Click();
+        Thread.Sleep(2000);
+
+        // Check if user has logged in successfully
+        IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+        if (helloHari.Text == "Hello hari!")
+        {
+            Console.WriteLine("User has logged in successfullly. Test passed!");
+        }
+        else
+        {
+            Console.WriteLine("User has not logged in. Test failed!");
+        }
+    }
+}
