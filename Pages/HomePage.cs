4a62b6f289c8e347c02b63a5c4a07778b7414165
Chandika Herath <chandikasbh@gmail.com>
2024-08-20 21:57:06 +1200

    Adding the code for week03 day 02 with Create a TImeMaterial record

---
+++ Pages/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnUpPortalWeek3And4.Utilities;
+
+namespace TurnUpPortalWeek3And4.Pages
+{
+    public class HomePage
+    {
+        public void NavigateToTMPage(IWebDriver driver)
+        {
+            // Navigate to Time and Material Page
+            IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+            administrationTab.Click();
+
+            Wait.WaitToBeClickable(driver, "XPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 10);
+
+            IWebElement timeAndMaterialOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            timeAndMaterialOption.Click();
+        }
+    }
+}
