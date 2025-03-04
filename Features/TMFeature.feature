db56d081abb7bc5bc7225b06a70ce9aa437dace7
Chandika Herath <chandikasbh@gmail.com>
2025-03-04 22:00:17 +1300

    Updated code with Reqnroll for BDD framework creation and added the basic feature file and StepDef for TimeAndMaterial feature

---
+++ Features/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a Turnup portal admin user
+I would like to create, edit and delete time and material records
+So that I can manage the employee time and materials successfully
+
+@regression @bvt @timeandmaterial
+Scenario: Create new time and material record with valid data
+	Given I logged into TurnUp portal successfully
+	And I navigate to the Time and Material page
+	When I create a new time and material record
+	Then the record should be created successfully
---
+++ StepDefinition/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using Reqnroll;
+
+namespace TurnUpPortalWeek3And4.StepDefinition
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given("I logged into TurnUp portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given("I navigate to the Time and Material page")]
+        public void GivenINavigateToTheTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I create a new time and material record")]
+        public void WhenICreateANewTimeAndMaterialRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("the record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
